5ec2d38cdf33a3b125cac2aea8d0e1702750a613
AK1239 <akilkhatri235@gmail.com>
2024-10-15 19:00:19 +0300

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check } from "lucide-react";
+import Phone from "@/components/Phone";
+import { Check, Star } from "lucide-react";
 
 export default function Home() {
   return (
@@ -21,7 +22,7 @@ export default function Home() {
                 className="relative w-fit tracking-tight text-balance mt-16
                font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl"
               >
-                Your Image on a<span className="bg-green-600 px-2 text-white">Custom</span> Phone
+                Your Image on a <span className="bg-green-600 px-2 text-white">Custom</span> Phone
                 Case
               </h1>
               <p
@@ -73,17 +74,49 @@ export default function Home() {
                     className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
                   />
                   <img
-                    src="/users/user-4.png"
+                    src="/users/user-4.jpg"
                     alt="user"
                     className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
                   />
                   <img
-                    src="/users/user-5.png"
+                    src="/users/user-5.jpg"
                     alt="user"
                     className="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-slate-100"
                   />
                 </div>
+
+                <div
+                  className="flex flex-col justify-between items-center
+                sm:items-start"
+                >
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1,250</span> happy customers
+                  </p>
+                </div>
               </div>
+            </div>
+          </div>
+
+          <div
+            className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0
+          mt-32 lg:mx-0 lg:mt-20 h-fit"
+          >
+            <div className="relative md:max-w-xl">
+              <img
+                src="/your-image.png"
+                alt="your image text"
+                className="absolute w-40 lg:w-52 left-56
+              -top-20 select-none hidden sm:block lg:hidden xl:block"
+              />
+              <img src="/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
