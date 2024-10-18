2598221702618fd2a8aa78973946f9d172dc5294
AK1239 <akilkhatri235@gmail.com>
2024-10-18 11:20:13 +0300

    materials in option validator

--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -1,3 +1,5 @@
+import { PRODUCT_PRICES } from "@/config/products";
+
 export const COLORS = [
   { label: "Black", value: "black", tw: "zinc-900" },
   { label: "Blue", value: "blue", tw: "blue-950" },
@@ -13,5 +15,23 @@ export const MODELS = {
     { label: "iPhone 13", value: "iphone13" },
     { label: "iPhone 14", value: "iphone14" },
     { label: "iPhone 15", value: "iphone15" },
+  ],
+} as const;
+
+export const MATERIALS = {
+  name: "materials",
+  options: [
+    {
+      label: "Silicone",
+      value: "silicone",
+      description: undefined,
+      price: PRODUCT_PRICES.material.silicone,
+    },
+    {
+      label: "Soft Polycarbonate",
+      value: "polycarbonate",
+      description: "Scratch-resistant coating",
+      price: PRODUCT_PRICES.material.polycarbonate,
+    },
   ],
 } as const;
