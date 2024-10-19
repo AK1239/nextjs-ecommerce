0b2b70ebe8db37902a5e8f778ae3a2acd29cb723
AK1239 <akilkhatri235@gmail.com>
2024-10-19 12:28:30 +0300

    fixed providers issue

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const client = new QueryClient();
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <QueryClientProvider client={client}></QueryClientProvider>;
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
 
 export default Providers;
